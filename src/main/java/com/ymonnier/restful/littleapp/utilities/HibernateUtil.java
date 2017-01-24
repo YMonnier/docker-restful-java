da8ac66075ab71e642652c1373a7f1823b9823f3
Ysee Monnier <yseemonnier@gmail.com>
2017-01-24 22:43:57 +0100

    add hibernate config layer

---
+++ src/main/java/com/ymonnier/restful/littleapp/utilities/HibernateUtil.java
@@ -0,0 +1,45 @@
+package com.ymonnier.restful.littleapp.utilities;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Project Project.
+ * Package com.ymonnier.restful.littleapp.utilities.
+ * File HibernateUtil.java.
+ * Created by Ysee on 24/01/2017 - 22:38.
+ * www.yseemonnier.com
+ * https://github.com/YMonnier
+ */
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    /**
+     * Create the SessionFactory from hibernate.cfg.xml
+     * @return A new SessionFactory
+     */
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable exception) {
+            System.err.println("Initial SessionFactory creation failed." + exception);
+            throw new ExceptionInInitializerError(exception);
+        }
+    }
+
+    /**
+     * Getter session factory.
+     * @return session factory.
+     */
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    /**
+     * Close the current session
+     * (Close caches and connection pools).
+     */
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
