8e0559c15c43be7d26ce0c864d2585cd1ba47981
Ysee Monnier <yseemonnier@gmail.com>
2017-01-27 09:10:07 +0100

    init client project

---
+++ web/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webApp
+ * @description
+ * # webApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
