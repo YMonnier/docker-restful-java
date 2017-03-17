fe248e356409b94131dc303e861b10bc340d19fd
Ysee Monnier <yseemonnier@gmail.com>
2017-03-17 20:07:35 +0100

    change grizzly version

--- server/src/main/java/com/ymonnier/restful/littleapp/Main.java
+++ server/src/main/java/com/ymonnier/restful/littleapp/Main.java
@@ -43,8 +43,10 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
+
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Stopping server..");
             server.stop();
